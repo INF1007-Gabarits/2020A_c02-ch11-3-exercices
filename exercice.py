8b7dbf2ce700ae5b50875f47d1e0a4633a149456
chosson <chosson@users.noreply.github.com>
2020-11-23 10:27:19 -0500

    Fixed bugs and better balanced the simulated battle.

--- exercice.py
+++ exercice.py
@@ -9,10 +9,10 @@ from inspect import *
 from game import *
 
 
-def main():
-	c1 = Character("Äpik", 400, 150, 70, 70)
+def simulate_battle():
+	c1 = Character("Äpik", 500, 150, 70, 70)
 	c2 = Character("Gämmör", 550, 100, 120, 60)
-	c3 = Magician("Damn! That magic dude", 400, 100, 50, 150, 50, 65)
+	c3 = Magician("Damn! That magic dude", 450, 100, 50, 150, 50, 65)
 
 	c1.weapon = Weapon("BFG", 100, 69)
 	c2.weapon = Weapon("Deku Stick", 120, 1)
@@ -20,8 +20,12 @@ def main():
 	c3.weapon = Weapon("Slingshot", 80, 20)
 	c3.using_magic = True
 
-	turns = run_battle(c3, c2)
+	turns = run_battle(c3, c1)
 	print(f"The battle ended in {turns} turns.")
+
+
+def main():
+	simulate_battle()
 
 if __name__ == "__main__":
 	main()
--- game.py
+++ game.py
@@ -13,6 +13,9 @@ from magician import *
 
 
 def deal_damage(attacker, defender):
+	# TODO: Obtenir l'arme utilisée.
+	#       Si l'attaquant est un magicien et que `Magician.will_use_spell()` donne vrai, il utilise sa magie.
+	#       Sinon il utilise son arme physique.
 	weapon_used = attacker.spell if isinstance(attacker, Magician) and attacker.will_use_spell() else attacker.weapon
 	damage, crit = attacker.compute_damage(defender)
 	defender.hp -= damage
